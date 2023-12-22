dd94ee2aa0a8c44cfa76f2ef4783990796cd25b3
Edmond <edpodlegaev@yandex.ru>
2023-12-22 17:49:34 +0300

    Initial Commit

---
+++ SparrowCode_EighthTask/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SparrowCode_EighthTask
+//
+//  Created by Edmond Podlegaev on 21.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SparrowCode_EighthTask/SparrowCode_EighthTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparrowCode_EighthTaskApp.swift
+//  SparrowCode_EighthTask
+//
+//  Created by Edmond Podlegaev on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparrowCode_EighthTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
